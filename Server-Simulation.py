ce8202f14b3aae6aee425ef4d07c415797b622b3
Joel Temiz <joel.temiz@unibw.de>
2022-02-17 14:02:23 +0100

    Optimize Server simulation

--- Server-Simulation.py
+++ Server-Simulation.py
@@ -3,6 +3,8 @@ import random
 import socket
 import time
 
+temps = [5, 5, 5, 5]
+median = 70
 
 def runServer():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,9 +19,18 @@ def runServer():
 
 def genMessage(index):
     str = "VALUE;{index};{time};{temp1};{temp2};{temp3};{temp4}"
+    changeTemps()
     return bytes(
-        str.format(index=index, time=datetime.datetime.now(), temp1=random.random() * 90, temp2=random.random() * 90, temp3=random.random() * 90,
-                   temp4=random.random() * 90), 'ascii')
+        str.format(index=index, time=datetime.datetime.now(), temp1=temps[0], temp2=temps[1], temp3=temps[2],
+                   temp4=temps[3]), 'ascii')
 
+def changeTemps():
+    global temps
+    for i in range(len(temps)):
+        if temps[i] < median * 1.05:
+            temps[i] += random.random()*5
+        else:
+            temps[i] -= random.random()*5
+    print(temps)
 
 runServer()
